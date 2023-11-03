835061159ab654fbac54911873a8dcd0f9a375a9
wilker-home <wilkersoares.wl@gmail.com>
2023-11-03 14:47:58 -0300

    script for animation

---
+++ Public/js/script.js
@@ -0,0 +1,70 @@
+//ANIMAÇÃO BUTTON INICIO
+var home = document.querySelector(".Inicio");
+
+home.onclick = function efeito() {
+   var e = document.getElementById("Inicio");
+   e.style.animation = "none";
+   setTimeout(function() {
+      e.style.animation = "";
+   }, 100);
+}
+
+var home = document.querySelector(".Antecedentes");
+
+home.onclick = function efeito() {
+   var e = document.getElementById("Antecedentes");
+   e.style.animation = "none";
+   setTimeout(function() {
+      e.style.animation = "";
+   }, 100);
+}
+
+var home = document.querySelector(".construcao");
+
+home.onclick = function efeito() {
+   var e = document.getElementById("Construção");
+   e.style.animation = "none";
+   setTimeout(function() {
+      e.style.animation = "";
+   }, 100);
+}
+
+var home = document.querySelector(".colonia");
+
+home.onclick = function efeito() {
+   var e = document.getElementById("Colônia-ao-Império");
+   e.style.animation = "none";
+   setTimeout(function() {
+      e.style.animation = "";
+   }, 100);
+}
+
+var home = document.querySelector(".periodo");
+
+home.onclick = function efeito() {
+   var e = document.getElementById("Periodo");
+   e.style.animation = "none";
+   setTimeout(function() {
+      e.style.animation = "";
+   }, 100);
+}
+
+var home = document.querySelector(".constituicao");
+
+home.onclick = function efeito() {
+   var e = document.getElementById("constituição");
+   e.style.animation = "none";
+   setTimeout(function() {
+      e.style.animation = "";
+   }, 100);
+}
+
+var home = document.querySelector(".caracter");
+
+home.onclick = function efeito() {
+   var e = document.getElementById("caracter");
+   e.style.animation = "none";
+   setTimeout(function() {
+      e.style.animation = "";
+   }, 100);
+}
